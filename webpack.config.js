5c93b0b858668956bd71800dba6c0d636fab412f
Jaime Muñoz <jaime.munoz200@gmail.com>
2022-04-03 10:31:12 -0600

    module rules loaders

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,19 @@ module.exports ={
     mode: 'development',
     module : {
         rules : [
+            {
+                test : /\.(js|jsx)$/,
+                exclude : /node_modules/,
+                use : { loader : 'babel-loader' }
+            },
+            {
+                test : /\.html$/,
+                use : [ {loader: 'html-loader'} ]
+            },
+            {
+                test: /\.(css)$/,
+                use : [ 'css-loader' ]
+            }
         ]
     },
     plugins : [],
