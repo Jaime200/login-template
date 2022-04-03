c8805f680d567dfd878806d45e9df46662812111
Jaime Muñoz <jaime.munoz200@gmail.com>
2022-04-03 12:28:20 -0600

    Login

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-ReactDOM.render(<>
-<h1>Hola mundo</h1>
-</>, document.getElementById('app'));
+import './styles/Login.css'
+import user from './assets/no-user.png'
+ReactDOM.render(
+    <div className="login">
+    <div className="login__container">
+        <img className="login__container--logo" src={user} alt="imagen"/>
+        <form className="login__container--form">
+            <label for="login">Login</label>
+            <input className="input" type="text" placeholder="jmunoz200"/>
+            <label for="password">Password</label>
+            <input className="input" type="password" placeholder="*******"/>
+            <button className="btn primary">Login</button>
+        </form>
+        <button className="btn secondary ">Forgot my password</button>
+    </div>
+</div>
+    , document.getElementById('app'));
